087ceabd8e93476526a669a2f8105a8b3c8706c7
Rene Lehbrunner <rene.leh@gmx.at>
2021-01-21 00:00:26 +0100

    ListView hinzugefügt

--- lib/screens/hairdresser_overview_screen.dart
+++ lib/screens/hairdresser_overview_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:studr/widgets/appBar.dart';
 import 'package:studr/widgets/categoriesScroller.dart';
-import 'package:studr/widgets/topAppBar.dart';
 
 import '../models/hairdresser.dart';
 import 'detail_screen.dart';
@@ -15,15 +14,64 @@ class ProductsOverviewScreen extends StatefulWidget {
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  final List<Hairdresser> loadedItems = [
+    Hairdresser(
+      id: 1,
+      title: 'Figarouno',
+      description: 'Neues Styling, kreative Schnitte oder klassisch.',
+      priceSection: '€',
+      icon: 'figarouno2.png',
+    ),
+    Hairdresser(
+      id: 2,
+      title: 'Klipp',
+      description: 'Neues Styling, kreative Schnitte oder klassisch',
+      priceSection: '€€€',
+      icon: 'Klipp.jpg',
+    ),
+    Hairdresser(
+      id: 3,
+      title: 'Sturmayr',
+      description: 'Neues Styling, kreative Schnitte oder klassisch.',
+      priceSection: '€€',
+      icon: 'Sturmayr2.jpg',
+    ),
+    Hairdresser(
+      id: 4,
+      title: 'Red Level',
+      description: 'Neues Styling, kreative Schnitte oder klassisch.',
+      priceSection: '€€',
+      icon: 'RedLevel1.png',
+    ),
+    Hairdresser(
+      id: 5,
+      title: 'Schnittzone',
+      description: 'Neues Styling, kreative Schnitte oder klassisch.',
+      priceSection: '€',
+      icon: 'Schnittzone.jpg',
+    ),
+    Hairdresser(
+      id: 6,
+      title: 'Fessler Barbershop',
+      description: 'Neues Styling, kreative Schnitte oder klassisch.',
+      priceSection: '€',
+      icon: 'fessler_barbershop.png',
+    ),
+    Hairdresser(
+      id: 7,
+      title: 'Stylepark',
+      description: 'Neues Styling, kreative Schnitte oder klassisch.',
+      priceSection: '€',
+      icon: 'Stylepark.png',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.cyan.shade50,
         bottomNavigationBar: MyAppBar(),
-        body: Container(
-          child: TopAppBar(),
-        ),
+        body: CategoriesScroller(this.loadedItems),
       ),
     );
   }
